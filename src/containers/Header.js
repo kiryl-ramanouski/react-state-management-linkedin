867bf65bc6e12edafcb28e80030dfa16a0afef46
Kirill Romanovskii <whitekvazar@gmail.com>
2022-08-22 14:01:11 +0300

    refactor: refactor all containers with prettier

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,5 +1,5 @@
-import { connect } from 'react-redux'
-import Header from '../components/Header'
-import { addBook } from '../actions'
+import { connect } from 'react-redux';
+import Header from '../components/Header';
+import { addBook } from '../actions';
 
-export default connect(null, { addBook })(Header)
+export default connect(null, { addBook })(Header);
--- src/containers/VisibleBookList.js
+++ src/containers/VisibleBookList.js
@@ -1,21 +1,17 @@
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import * as TodoActions from '../actions'
-import BookList from '../components/BookList'
-import { getVisibleBookList } from '../selectors'
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as TodoActions from '../actions';
+import BookList from '../components/BookList';
+import { getVisibleBookList } from '../selectors';
 
-const mapStateToProps = state => ({
-  filteredTodos: getVisibleBookList(state)
-})
+const mapStateToProps = (state) => ({
+  filteredTodos: getVisibleBookList(state),
+});
 
-const mapDispatchToProps = dispatch => ({
-  actions: bindActionCreators(TodoActions, dispatch)
-})
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators(TodoActions, dispatch),
+});
 
+const VisibleBookList = connect(mapStateToProps, mapDispatchToProps)(BookList);
 
-const VisibleBookList = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(BookList)
-
-export default VisibleBookList
+export default VisibleBookList;
